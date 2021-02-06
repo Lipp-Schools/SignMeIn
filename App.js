f9305251b50fed69d1d385b9360f1d1429bc6359
Tarek Ali <tarekali15@outlook.com>
2021-02-06 10:46:39 -0600

    useState, app styles, workin on it

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [mother, setMother] = useState("Mother");
+  const [father, setFather] = useState("Father");
+  const [child, setChild] = useState("Child");
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>Register</Text>
+      <View style={styles.input}>
+        <TextInput />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#050a30',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#050a30",
+    alignItems: "center",
+  },
+  header: {
+    color: "#ffffff",
+    fontSize: 50,
+    paddingTop: 50,
   },
 });
