55477fe50c7441d9bb1fa26993157e4c081e5aa0
Tarek Ali <tarekali15@outlook.com>
2021-02-07 09:38:12 -0600

    first page and module update;

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Button, SafeAreaView, Text, View } from "react-native";
 import Styles from "./utils/Styles";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import Register from "./pages/Register";
 
 const Stack = createStackNavigator();
 
@@ -10,7 +11,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        
+        <Stack.Screen name="Register" component={Register} />
       </Stack.Navigator>
     </NavigationContainer>
   );
