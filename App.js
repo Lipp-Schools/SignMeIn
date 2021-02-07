faee2aa584d5d43a7c54eaf58df2264138a3585c
Tarek Ali <tarekali15@outlook.com>
2021-02-07 09:27:38 -0600

    new deps

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, SafeAreaView, Text, View } from "react-native";
 import Styles from "./utils/Styles";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return <SafeAreaView style={Styles.container}></SafeAreaView>;
