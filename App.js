8381b133d9f1ce5edf6dd1a30a76cc3170169d48
Tarek Ali <tarekali15@outlook.com>
2021-02-07 09:35:30 -0600

    working on adding navigation

--- App.js
+++ App.js
@@ -2,7 +2,16 @@ import React from "react";
 import { Button, SafeAreaView, Text, View } from "react-native";
 import Styles from "./utils/Styles";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <SafeAreaView style={Styles.container}></SafeAreaView>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
