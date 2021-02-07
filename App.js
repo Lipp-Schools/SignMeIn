92cf659ed6dafcb5e727b94e7ea5915d45ab50f6
Tarek Ali <tarekali15@outlook.com>
2021-02-07 09:45:27 -0600

    removed residual router call

--- App.js
+++ App.js
@@ -4,10 +4,5 @@ import Styles from "./utils/Styles";
 import Register from "./pages/Register";
 
 export default function App() {
-  const Stack = createStackNavigator();
-  return (
-    <SafeAreaView style={Styles.container}>
-
-    </SafeAreaView>
-  );
+  return <SafeAreaView style={Styles.container}></SafeAreaView>;
 }
