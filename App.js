2d4a4f8d0849cb5ffe7e88cbaf8b708a302c522c
Tarek Ali <tarekali15@outlook.com>
2021-02-08 19:03:03 -0600

    removed routing; going to redesign register page

--- App.js
+++ App.js
@@ -2,24 +2,11 @@ import React from "react";
 import { Button, SafeAreaView } from "react-native";
 import Styles from "./utils/Styles";
 import Register from "./pages/Register";
-import { NavigationContainer, NavigationCotainer, StackActions } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import Home from "./pages/Home";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home"
-          component={Home} 
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-    // <SafeAreaView style={Styles.hyperlink}>
-    //   <Button title="Register" color="#ffffff" onPress={} />
-    //   <Button title="Sign In" color="#ffffff" />
-    // </SafeAreaView>
+    <Register />
   );
 }
--- pages/Home.js
+++
@@ -1,9 +0,0 @@
-import Register from "./Register";
-import React from "react";
-import { Button } from "react-native";
-
-export default Home = ({ navigation }) => {
-  return (
-    <Button title="Register" onPress={() => navigation.navigate(Register)} />
-  );
-};
