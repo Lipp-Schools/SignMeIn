20929aca561160a449c8b70510c1b5c8658aa6b4
Tarek Ali <tarekali15@outlook.com>
2021-02-15 12:59:13 -0600

    more firebase logic; some invoked in App.js

--- App.js
+++ App.js
@@ -4,9 +4,10 @@ import Styles from "./utils/Styles";
 import Register from "./pages/Register";
 import { readFile } from "fs";
 import { info } from "console";
-
+import InitFirebase  from "./utils/firebase/InitFirebase";
 
 export default function App() {
+  InitFirebase();
   return (
     <Register />
   );
