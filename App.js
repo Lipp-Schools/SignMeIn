57c369cfde2dd9871fa2e7b303e291fd0646c3b6
Tarek Ali <tarekali15@outlook.com>
2021-02-15 13:56:50 -0600

    removed redundant imports in App.js; also working to fix an issue with UUID generation

--- App.js
+++ App.js
@@ -2,12 +2,8 @@ import React from "react";
 import { Button, SafeAreaView } from "react-native";
 import Styles from "./utils/Styles";
 import Register from "./pages/Register";
-import { readFile } from "fs";
-import { info } from "console";
-import InitFirebase from "./utils/firebase/Firebase";
 
 export default function App() {
-  InitFirebase();
   return (
     <Register />
   );
--- utils/firebase/CreateUser.js
+++ utils/firebase/CreateUser.js
@@ -1,8 +1,8 @@
 import firebase from "./Firebase";
-import { v4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 export default CreateUser = (user) => {
-    firebase.firestore().collection("users").doc(v4()).set({
+    firebase.firestore().collection("users").doc(uuidv4()).set({
         campus: user.campus,
         child: user.child,
         father: user.father,
