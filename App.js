4489cd73d63806aa86206d75fe2f126e322dbd4f
Tarek Ali <tarekali15@outlook.com>
2021-02-19 11:07:09 -0600

    return statement

--- App.js
+++ App.js
@@ -5,14 +5,16 @@ import Register from "./pages/Register";
 import { NativeRouter, Route, Link } from "react-router-native";
 
 export default function App() {
-  <NativeRouter>
-    <View style={Styles.container}>
-      <View style={Styles.inputWrap}>
-        <Link to="/register" style={Styles.button}>
-          <Text style={Styles.buttonText}>Register</Text>
-        </Link>
+  return (
+    <NativeRouter>
+      <View style={Styles.container}>
+        <View style={Styles.inputWrap}>
+          <Link to="/register" style={Styles.button}>
+            <Text style={Styles.buttonText}>Register</Text>
+          </Link>
+        </View>
       </View>
-    </View>
-    <Route exact path="/register" component={Register} />
-  </NativeRouter>;
+      <Route exact path="/register" component={Register} />
+    </NativeRouter>
+  );
 }
