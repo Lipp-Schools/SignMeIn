53b30b7a47e7dd0683938e0e320b6978d5f69e02
Tarek Ali <tarekali15@outlook.com>
2021-02-19 10:53:33 -0600

    working on routing

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { Button, SafeAreaView } from "react-native";
+import { View } from "react-native";
 import Styles from "./utils/Styles";
 import Register from "./pages/Register";
+import { NativeRouter, Route, Link } from "react-router-native";
 
 export default function App() {
-  return <Register />;
+  <NativeRouter>
+    <View style={Styles.container}>
+      <View style={Styles.inputWrap}>
+        <Link to="/register" style={Styles.button}>
+          <Text style={Styles.buttonText}>Register</Text>
+        </Link>
+      </View>
+    </View>
+  </NativeRouter>;
 }
