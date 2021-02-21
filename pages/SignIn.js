02cdbe29804acd84422270b92df882458ae8ab01
Tarek Ali <tarekali15@outlook.com>
2021-02-21 10:22:59 -0600

    search component in SignIn.js

--- pages/SignIn.js
+++ pages/SignIn.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, View, Text } from "react-native";
+import { SafeAreaView, View, Text, TextInput } from "react-native";
 import Styles from "../utils/Styles";
 import React from "react";
 
@@ -9,7 +9,11 @@ export default function SignIn() {
         <Text style={Styles.header}>Sign In</Text>
       </View>
       <View style={Styles.inputWrap}>
-
+        <TextInput
+          style={Styles.input}
+          placeholder={"Search"}
+          selectionColor="#000000"
+        />
       </View>
     </SafeAreaView>
   );
