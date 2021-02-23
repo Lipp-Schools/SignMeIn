e6fab28fc33352a949c3891e0bff6991d16cca84
Tarek Ali <tarekali15@outlook.com>
2021-02-23 16:45:14 -0600

    fixed asynchrony issue

--- utils/firebase/ReadUsers.js
+++ utils/firebase/ReadUsers.js
@@ -3,11 +3,9 @@ import React from "react";
 import { db } from "./Firebase";
 
 export default function ReadUsers() {
-  const users = db.collection("users").doc();
-
-  users.get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      console.log(doc.id, " => ", doc.data());
+  db.collection("users")
+    .get()
+    .then((snapshot) => {
+      console.log(snapshot.docs);
     });
-  });
 }
