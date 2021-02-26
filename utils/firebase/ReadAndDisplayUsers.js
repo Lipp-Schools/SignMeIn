1df30bdd8ced6f920de237816d081bf52d6a1656
Tarek Ali <tarekali15@outlook.com>
2021-02-26 10:14:44 -0600

    ReadAndDisplayUsers appending seems to be functioning

--- utils/firebase/ReadAndDisplayUsers.js
+++ utils/firebase/ReadAndDisplayUsers.js
@@ -3,15 +3,23 @@ import React from "react";
 import { db } from "./Firebase";
 
 export default function ReadAndDisplayUsers() {
-  const users = [];
-
+  const displayArray = [];
   db.collection("users")
     .get()
     .then((snapshot) => {
       snapshot.forEach((user) => {
-        users.push({ id: user.id, data: user.data });
+        console.log({
+          campus: user.data().campus,
+          child: user.data().child,
+          father: user.data().father,
+          mother: user.data().mother,
+        });
+        displayArray.push({
+          campus: user.data().campus,
+          child: user.data().child,
+          father: user.data().father,
+          mother: user.data().mother,
+        });
       });
     });
-  
-    console.log(users);
 }
