f44ea9d4010416945a0ffd6701fd9aa765a20110
Tarek Ali <tarekali15@outlook.com>
2021-03-02 16:36:23 -0600

    added confirm sign in. working on it...

---
+++ pages/ConfirmSignIn.js
@@ -0,0 +1,22 @@
+import { SafeAreaView, View, Text, TextInput, Modal } from "react-native";
+import Styles from "../utils/Styles";
+import React from "react";
+import ReadAndDisplayUsers from "../utils/firebase/ReadAndDisplayUsers";
+
+export default function ConfirmSignIn() {
+  return (
+    <SafeAreaView style={Styles.container}>
+      <View style={Styles.headerWrap}>
+        <Text style={Styles.header}>Sign In</Text>
+      </View>
+      <View style={Styles.inputWrap}>
+        <TextInput
+          style={Styles.input}
+          placeholder={"Search"}
+          selectionColor="#000000"
+        />
+        <ReadAndDisplayUsers />
+      </View>
+    </SafeAreaView>
+  );
+}
